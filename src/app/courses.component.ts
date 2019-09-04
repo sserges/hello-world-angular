07e77b639f816a3d662c4b9307b8090800ff9f7f
sserges <agounkeserge@gmail.com>
2019-09-04 19:26:04 +0000

    Style binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,21 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'app-courses',
     template: `
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white'" >Save</button>
     `
 })
 export class CoursesComponent {
-    isActive = false;
-    title = 'List of courses';
-    courses;
-    imageUrl = 'http://lorempixel.com/400/200';
-    colSpan = 2;
-
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses();
-    }
-
-    getTitle() {
-        return this.title;
-    }
+    isActive = true;
 }
