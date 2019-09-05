d8b25798073027e3bf020ad26db13cd8bcc7e26a
sserges <agounkeserge@gmail.com>
2019-09-05 18:45:00 +0000

    Event Filtering

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,11 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'app-courses',
     template: `
-        <div (click)="onDivClicked()">
-            <button (click)="onSave($event)">Save</button>
-        </div>
+        <input (keyup.enter)="onKeyUp()" />
     `
 })
 export class CoursesComponent {
-
-    onDivClicked() {
-        console.log('Div was clicked');
-    }
-
-    onSave($event) {
-        $event.stopPropagation();
-        console.log($event);
+    onKeyUp() {
+        console.log('ENTER was pressed');
     }
 }
