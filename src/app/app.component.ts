85a83eb78ff49543d4c629d692b6461d6fdaeb88
sserges <agounkeserge@gmail.com>
2019-10-11 16:16:17 +0000

    ngFor and Trackby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,17 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
-    { id: 1, name: 'course1 ' },
-    { id: 2, name: 'course2 ' },
-    { id: 3, name: 'course3 ' },
-  ];
+  courses;
 
-  onAdd() {
-    this.courses.push({ id: 4, name: 'course4' });
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1 ' },
+      { id: 2, name: 'course2 ' },
+      { id: 3, name: 'course3 ' },
+    ];
   }
 
-  onChange(course) {
-    course.name = 'UPDATED';
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
   }
 }
