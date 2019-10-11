0373fac73025337bf0b41166b1f413bf3a24ad87
sserges <agounkeserge@gmail.com>
2019-10-11 15:00:25 +0000

    ngFor and Change Detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,12 @@ export class AppComponent {
     { id: 2, name: 'course2 ' },
     { id: 3, name: 'course3 ' },
   ];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' });
+  }
+
+  onChange(course) {
+    course.name = 'UPDATED';
+  }
 }
