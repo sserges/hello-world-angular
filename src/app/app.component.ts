b031035a183a594c04b39794effdec31ee55f7cc
sserges <agounkeserge@gmail.com>
2019-10-11 18:17:52 +0000

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,5 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses;
-
-  loadCourses() {
-    this.courses = [
-      { id: 1, name: 'course1 ' },
-      { id: 2, name: 'course2 ' },
-      { id: 3, name: 'course3 ' },
-    ];
-  }
-
-  trackCourse(index, course) {
-    return course ? course.id : undefined;
-  }
+  canSave = false;
 }
