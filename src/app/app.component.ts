dc1ee28a7d7ff6c39722a216695410ba6aa5b275
sserges <agounkeserge@gmail.com>
2019-10-12 19:43:57 +0000

    safe traversal operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,8 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  canSave = false;
+  task = {
+    title: 'Review applications',
+    assignee: null
+  };
 }
