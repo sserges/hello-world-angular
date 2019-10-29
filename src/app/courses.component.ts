f3fe1d34bfa27a28b45f64c495fe3975b744dab8
sserges <agounkeserge@gmail.com>
2019-10-29 19:27:27 +0000

    RouterOutlet

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
+import { CoursesService } from './services/courses.service';
 
 @Component({
     selector: 'app-courses',
--- src/app/services/github-followers.service.ts
+++ src/app/services/github-followers.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { DataService } from './services/data.service';
+import { DataService } from './data.service';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
