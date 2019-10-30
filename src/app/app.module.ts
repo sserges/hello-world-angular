e718939053fd2a4d850cccf350c445ab48237fb5
sserges <agounkeserge@gmail.com>
2019-10-30 17:23:32 +0000

    Getting the Route Parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -68,7 +68,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
         component: HomeComponent
       },
       {
-        path: 'followers/:username',
+        path: 'followers/:id',
         component: GithubProfileComponent
       },
       {
